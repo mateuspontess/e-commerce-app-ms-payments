51ab197fc29208776583a2b4a0cab3018a25d37e
Mateus Pontes <mateuspsdd@gmail.com>
2024-05-14 17:35:34 -0300

    Add `receiveQueueMessagesOrder` method to PaymentListener

--- src/main/java/br/com/ecommerce/payment/amqp/PaymentListener.java
+++ src/main/java/br/com/ecommerce/payment/amqp/PaymentListener.java
@@ -1,9 +1,13 @@
 package br.com.ecommerce.payment.amqp;
 
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
+import br.com.ecommerce.payment.model.PaymentDTO;
 import br.com.ecommerce.payment.service.PaymentService;
+import jakarta.transaction.Transactional;
 
 @Component
 public class PaymentListener {
@@ -11,4 +15,9 @@ public class PaymentListener {
 	@Autowired
 	private PaymentService service;
 
+	@RabbitListener(queues = "payments.details-order")
+	@Transactional
+	public void receiveQueueMessagesOrder(@Payload PaymentDTO dto) {
+		service.createPayment(dto);
+	}
 }
