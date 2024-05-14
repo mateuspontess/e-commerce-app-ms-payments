4917b861fc85e191878e19e0e612be6f3bb8bb59
Mateus Pontes <mateuspsdd@gmail.com>
2024-05-14 17:45:06 -0300

    Add `createPayment` method

--- src/main/java/br/com/ecommerce/payment/service/PaymentService.java
+++ src/main/java/br/com/ecommerce/payment/service/PaymentService.java
@@ -3,6 +3,9 @@ package br.com.ecommerce.payment.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.ecommerce.payment.model.Payment;
+import br.com.ecommerce.payment.model.PaymentDTO;
+import br.com.ecommerce.payment.model.PaymentStatus;
 import br.com.ecommerce.payment.repository.PaymentRepository;
 
 @Service
@@ -10,4 +13,15 @@ public class PaymentService {
 	
 	@Autowired
 	private PaymentRepository repository;
+	
+	
+	public void createPayment(PaymentDTO dto) {
+		Payment payment = new Payment(
+				dto.orderId(), 
+				dto.userId(), 
+				dto.paymentAmount(), 
+				PaymentStatus.AWAITING);
+		
+		repository.save(payment);
+	}
 }
