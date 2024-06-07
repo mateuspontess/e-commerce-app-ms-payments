1b89c72491944491c1cec73ddf008be8643b22a3
Mateus Pontes <mateuspsdd@gmail.com>
2024-06-07 17:39:22 -0300

    refactor: slight refactoring

--- src/main/java/br/com/ecommerce/payment/configs/StringToCategoryConverter.java
+++ src/main/java/br/com/ecommerce/payment/configs/StringToCategoryConverter.java
@@ -1,13 +1,14 @@
 package br.com.ecommerce.payment.configs;
 
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.NonNull;
 
 import br.com.ecommerce.payment.model.PaymentStatus;
 
 public class StringToCategoryConverter implements Converter<String, PaymentStatus>{
 
 	@Override
-	public PaymentStatus convert(String source) {
+	public PaymentStatus convert(@NonNull String source) {
 		try {
 			return PaymentStatus.fromString(source);
 		
