93eea368b4198b062b8d28dd518299f609032106
Mateus Pontes <mateuspsdd@gmail.com>
2024-06-07 14:57:39 -0300

    ...

--- src/test/java/br/com/ecommerce/payment/PaymentApplicationTests.java
+++ src/test/java/br/com/ecommerce/payment/PaymentApplicationTests.java
@@ -2,9 +2,15 @@ package br.com.ecommerce.payment;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
+import br.com.ecommerce.payment.PaymentApplicationTests.NoContext;
+
+@ContextConfiguration(classes = NoContext.class)
 @SpringBootTest
 class PaymentApplicationTests {
+
+	static class NoContext{}
 
 	@Test
 	void contextLoads() {
