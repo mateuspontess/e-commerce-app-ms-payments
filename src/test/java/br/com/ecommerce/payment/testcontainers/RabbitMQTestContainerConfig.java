e4898af1e228fea3725d989f59c60c508bfbd47f
Mateus Pontes <mateuspsdd@gmail.com>
2024-06-21 15:22:18 -0300

    test: refactor: `PaymentApplicationTests`

--- src/test/java/br/com/ecommerce/payment/testcontainers/RabbitMQTestContainerConfig.java
+++ src/test/java/br/com/ecommerce/payment/testcontainers/RabbitMQTestContainerConfig.java
@@ -18,7 +18,6 @@ public class RabbitMQTestContainerConfig {
         try (RabbitMQContainer rabbit = new RabbitMQContainer("rabbitmq:3.7.25-management-alpine")
                 .withExposedPorts(5672, 15672)) {
             registry.add("spring.rabbitmq.port", () -> rabbit.getAmqpPort());
-            registry.add("eureka.client.enabled", () -> false);
 
             return rabbit;
         }
